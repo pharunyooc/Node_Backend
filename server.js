8fdbab772d0d5f45c3e32ed7e1895b153c776455
PJ <Pharunyooc@gmail.com>
2022-04-05 05:17:51 +0700

    init

--- server.js
+++ server.js
@@ -1,9 +1,60 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8080
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+const secret = 'pts2021'
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'user'
+});
+
+
+app.use(cors(corsOptions));
+
 
 app.get("/",(req,res)=>{
     res.json({result:"ok",data:[1,2,3]})
+})
+app.post('/register', jsonParser, (req, res, next)=> {
+    let fname = req.body.fname;
+    let lname = req.body.lname;
+    let email = req.body.email;
+    let tel = req.body.tel;
+    let card = req.body.card;
+    let password = req.body.password;
+
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+        connection.execute(
+            'INSERT INTO user_db (fullname,email,tel,pid,password,status,position) VALUES(?,?,?,?,?,?,?)',
+            [fname + " " + lname,
+            email,
+            tel,
+            card,
+            hash,
+            'Disable',
+            'Admin'],
+            function (err, results, fields) {
+                if (err) {
+                    res.json({ status: '1', message: err.code })
+                    return;
+                } else {
+                    res.json({ status: 'ok'})
+                    return;
+                }
+            }
+        )
+    });
+
 })
 
 
