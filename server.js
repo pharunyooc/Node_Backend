7e374b3cebe72e7fc7d0d7769a3ef54b5087a0bf
PJ <Pharunyooc@gmail.com>
2022-04-05 05:05:09 +0700

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.get("/",(req,res)=>{
+    res.json({result:"ok",data:[1,2,3]})
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${{PORT}}`)
+
+})
